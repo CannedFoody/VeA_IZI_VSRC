1cd8493f208d5520d305c633bb4738498582f527
CannedFoody <gamingman72069@gmail.com>
2026-06-04 15:46:22 +0300

    User model class finished.

---
+++ VeA_IZI_VSRC/src/main/java/eu/virac/vea_izi_vsrc/model/User.java
@@ -0,0 +1,45 @@
+package eu.virac.vea_izi_vsrc.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table
+@Entity
+
+public class User {
+    @Column(name="idUser")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Setter(value=AccessLevel.NONE) private long idUser;
+
+    @Column(name="Name")
+    @NotNull
+    @NotEmpty
+    @Pattern(regexp = "[A-Ž]{1}[a-ž]{2,20}([ ]{1}([A-Ž]{1}[a-ž]{2,20}))?")
+    private String name;
+
+    @Column(name="Surname")
+    @NotNull
+    @NotEmpty
+    @Pattern(regexp = "[A-Ž]{1}[a-ž]{2,20}([ ]{1}([A-Ž]{1}[a-ž]{2,20}))?")
+    private String surname;
+
+    @Column(name="Email")
+    @NotNull
+    @NotEmpty
+    @Pattern(regexp = "^((?!\\.)[\\w\\-_.]*[^.])(@\\w+)(\\.\\w+(\\.\\w+)?[^.\\W])$")
+    private String email;
+
+    @Column(name="Role")
+    @NotNull
+    @NotEmpty
+    @Pattern(regexp = "\"[A-Ž]{1}[a-ž]{2,20}([ ]{1}([A-Ž]{1}[a-ž]{2,20}))?\"")
+    private String role;
+}
+
