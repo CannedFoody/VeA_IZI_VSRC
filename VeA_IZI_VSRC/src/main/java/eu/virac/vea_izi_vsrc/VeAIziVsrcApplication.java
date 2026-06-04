88f6869daed810a1ca1511e0b90728890408b9ad
CannedFoody <gamingman72069@gmail.com>
2026-06-04 15:20:10 +0300

    Spring Boot project creation.

---
+++ VeA_IZI_VSRC/src/main/java/eu/virac/vea_izi_vsrc/VeAIziVsrcApplication.java
@@ -0,0 +1,13 @@
+package eu.virac.vea_izi_vsrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeAIziVsrcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeAIziVsrcApplication.class, args);
+    }
+
+}
---
+++ VeA_IZI_VSRC/src/test/java/eu/virac/vea_izi_vsrc/VeAIziVsrcApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.virac.vea_izi_vsrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeAIziVsrcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
