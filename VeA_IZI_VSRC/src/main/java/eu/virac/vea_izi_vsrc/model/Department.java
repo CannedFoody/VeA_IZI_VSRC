206d6bf19360a79fc14e6e10b7b29a161b46829a
NormundsBrizs <normundsbrizs@gmail.com>
2026-06-04 16:03:22 +0300

    Edit: cleaned up code

--- VeA_IZI_VSRC/src/main/java/eu/virac/vea_izi_vsrc/model/Department.java
+++ VeA_IZI_VSRC/src/main/java/eu/virac/vea_izi_vsrc/model/Department.java
@@ -1,19 +1,8 @@
 package eu.virac.vea_izi_vsrc.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
 @Getter
 @Setter
