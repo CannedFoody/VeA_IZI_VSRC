c1cae976e4f260791a13886d6a48ea2cf04451bd
NormundsBrizs <normundsbrizs@gmail.com>
2026-06-23 19:19:01 +0300

    Add: added KPI CRUD service and implementation

---
+++ VeA_IZI_VSRC/src/main/java/eu/virac/vea_izi_vsrc/service/ICRUDKPIService.java
@@ -0,0 +1,23 @@
+package eu.virac.vea_izi_vsrc.service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import eu.virac.vea_izi_vsrc.model.Category;
+import eu.virac.vea_izi_vsrc.model.KPI;
+import eu.virac.vea_izi_vsrc.model.User;
+import eu.virac.vea_izi_vsrc.model.Enums.KPIStatus;
+
+public interface ICRUDKPIService {
+
+	public abstract KPI createNewKPI(KPI kpi) throws Exception;
+	
+	public abstract ArrayList<KPI> getAllKPIs() throws Exception;
+	
+	public abstract KPI getKPIById(long idKPI) throws Exception;
+	
+	public abstract KPI updateKPI(long idKPI, LocalDate creationDate, String title, String description, 
+			KPIStatus status, Category category, User creator, User overlooker) throws Exception;
+	
+	public abstract void deleteKPI(long idKPI) throws Exception;
+}
