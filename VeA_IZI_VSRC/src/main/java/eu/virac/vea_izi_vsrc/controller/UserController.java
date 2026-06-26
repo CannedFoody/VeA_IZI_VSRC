6adb4921eb2f4256953243de29db1738bdc2df50
MikuChanz <mikusvalts@gmail.com>
2026-06-27 01:25:42 +0300

    edited UserController, removed filter functions and removed functions that shouldnt be used by a normal user

--- VeA_IZI_VSRC/src/main/java/eu/virac/vea_izi_vsrc/controller/UserController.java
+++ VeA_IZI_VSRC/src/main/java/eu/virac/vea_izi_vsrc/controller/UserController.java
@@ -5,9 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import eu.virac.vea_izi_vsrc.model.User;
 import eu.virac.vea_izi_vsrc.service.ICRUDUserService;
-import eu.virac.vea_izi_vsrc.service.IFilterUserService;
 
 @Controller
 @RequestMapping("/User")
@@ -16,17 +14,6 @@ public class UserController {
     @Autowired
     private ICRUDUserService userService;
 
-    @Autowired
-    private IFilterUserService filterUserService;
-
-    @PostMapping("/create")
-    public ResponseEntity<?> createUser(@RequestBody User user) {
-        try {
-            return ResponseEntity.ok(userService.createNewUser(user));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
 
     @GetMapping("/all")
     public ResponseEntity<?> getAllUsers() {
@@ -46,56 +33,4 @@ public class UserController {
         }
     }
 
-    @PutMapping("/update/{id}")
-    public ResponseEntity<?> updateUser(
-            @PathVariable long id,
-            @RequestBody User user) {
-        try {
-            return ResponseEntity.ok(
-                    userService.updateUser(
-                            id,
-                            user.getName(),
-                            user.getSurname(),
-                            user.getEmail(),
-                            user.getRole(),
-                            user.getDepartment()
-                    )
-            );
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable long id) {
-        try {
-            userService.deleteUser(id);
-            return ResponseEntity.ok("User deleted successfully.");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
-
-    @GetMapping("/filter/role")
-    public ResponseEntity<?> filterUsersByRole(@RequestParam String role) {
-        try {
-            return ResponseEntity.ok(
-                    filterUserService.filterUsersByRole(role)
-            );
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
-
-    @GetMapping("/filter/department/{departmentId}")
-    public ResponseEntity<?> filterUsersByDepartment(
-            @PathVariable long departmentId) {
-        try {
-            return ResponseEntity.ok(
-                    filterUserService.filterUsersByDepartmentId(departmentId)
-            );
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
 }
